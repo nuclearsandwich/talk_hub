043c8578943e9a55f4b231e382037d1c075f1a5f
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-04-13 10:29:13 -0700

    Let's make a TalkHub.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkHub::Application.config.secret_token = 'd0008a79c864119e184a167d7f982679a104c317ba43744bd79d0b84a9d68c7151848f63d19b99b04078da438edc6783b977149443f2b46dccf04552f65d0f04'
