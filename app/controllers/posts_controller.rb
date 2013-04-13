3a7164bee23bd279062a9b8bfdbc850e2c92954d
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-04-13 16:24:19 -0700

    Almost a TalkHub.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :only => [:create, :new]
+
   def index
     @posts = Post.all
   end
@@ -18,6 +20,19 @@ class PostsController < ApplicationController
       redirect_to show_post_path(@post)
     else
       render :action => :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    if @post.update_attributes(params[:post])
+      redirect_to show_post_path(@post)
+    else
+      render :action => :edit
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ TalkHub::Application.routes.draw do
   get "/posts/new" => "posts#new", :as => "new_post"
   post "/posts" => "posts#create", :as => "posts"
   get "/posts/:post_id" => "posts#show", :as => "show_post"
+  get "/posts/:post_id/edit" => "posts#edit", :as => "edit_post"
+  put "/posts/:post_id" => "posts#update", :as => "post"
 
   root :to => "home#index"
 end
